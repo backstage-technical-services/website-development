e3579728716c077fba722bb1b2ca10fd75c6fb4d
Ben Jones <ben@bnjns.uk>
2021-05-01 17:31:03 +0100

    improve visibility of errors when syncing repos

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -5,10 +5,13 @@ repo_base_dir="$(pwd)/$(dirname $0)/.."
 cd "${repo_base_dir}"
 
 if [[ ! $(ssh-keygen -F github.com) ]]; then
-    ssh-keyscan -H github.com >> ~/.ssh/known_hosts &> /dev/null
+    echo ":: Adding SSH keys for github.com"
+    ssh-keyscan -H github.com >> ~/.ssh/known_hosts
 fi
 
 repository_file="repositories.json"
+
+echo ":: Syncing repositories"
 
 for repository in $(jq -r '.[] | @base64' "${repository_file}"); do
     repository_decoded=$(echo "${repository}" | base64 --decode)
@@ -16,11 +19,20 @@ for repository in $(jq -r '.[] | @base64' "${repository_file}"); do
     path="$(echo "${repository_decoded}" | jq -r '.path')"
     url="$(echo "${repository_decoded}" | jq -r '.url')"
 
+    echo "::: ${path}"
+
     if [[ ! -d "${path}" ]]; then
-        echo "Cloning ${path} from ${url}"
-        git clone "${url}" "${path}" &> /dev/null
-        pushd "${path}" &> /dev/null && git checkout master &> /dev/null && popd &> /dev/null
+        echo ":::: Cloning from ${url}"
+        git clone "${url}" "${path}"
+        
     else
-        echo "Skipping ${path} as it already exists"
+        echo ":::: Skipping cloned as repo is already cloned"
     fi
+
+    echo ":::: Pulling latest from master"
+    pushd "${path}" 1> /dev/null
+    git checkout master && git pull --ff-only
+    popd 1> /dev/null
 done
+
+echo ":: Done"
